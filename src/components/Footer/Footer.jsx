622530ca805a9b35ecf664e81e7a66b788515fb2
Akash Agarwal <akashbittu96@gmail.com>
2020-05-10 15:10:06 +0530

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <div className={ styles.container}>
+           <span><b> STAY HOME,</b></span>
+           <span><b> STAY SAFE</b></span>
+        </div>
+    )
+}
+
+
+export default Footer;
